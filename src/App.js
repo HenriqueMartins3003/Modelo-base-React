884f69564f8f4c184b626514a7816861b336bf6c
Henrique Martins <hiikemartins@gmail.com>
2023-05-19 09:57:21 -0300

    Primeiro commit do app com React

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+
+import Login from './pages/Login';
+import GlobalStyles from './styles/GlobalStyles';
+import Header from './components/Header';
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Login />
+            <GlobalStyles />
+        </>
+    );
+}
+
+export default App;
